3132904592493f4d2c8ccda4d85f62a9da393ae0
Arcadi Garcia <arcadigarciarius21@gmail.com>
2017-06-08 21:55:42 +0200

    Simulacions numèriques
    Funciona????

--- numerical/lissajuste.m
+++ numerical/lissajuste.m
@@ -1,39 +1,44 @@
-numpasos1 = 1000;
-numpasos2 = 1000;
+numpasos1 = 100;
+numpasos2 = 200;
 
 p = 110e-6;
 lambda = 550e-9;
 tam = 2.07e-3;
 zt = 2*p^2/lambda;
 z1 = 50e-2;
-z2 = 2*zt;
+z2 = zt;
 f = 5e-2;
+zl = 50e-5;
 
 %Para varios casos de descolimación
-dz = linspace(-zt,zt,numpasos1);
+dz = linspace(-zl,zl,numpasos1);
 gamma = zeros(size(dz));
-for n=1:size(gamma)
-    gamma(n) = (2*f^2+dz(n)*(2*f-z1))./(2*f^2+dz(n)*(2*f-z2));
+for n=1:numpasos1
+    a = 2*f-z1;
+    a = 2*f^2+dz(n)*a;
+    gamma(n) = a./(a+dz(n)*z2);
 end
 
-x = zeros(numpasos2);
-y = zeros(numpasos2);
-eli = zeros(numpasos1);
+x = zeros(numpasos2,1);
+y = zeros(numpasos2,1);
+eli = zeros(numpasos1,1);
+eps = zeros(numpasos1,1);
 
 critm = round(numpasos1/2);
-critt = numpasos1-1;
+critt = numpasos1;
 
 tope = zeros(numpasos2,2);
 lowe = zeros(numpasos2,2);
 mide = zeros(numpasos2,2);
 phi = linspace(0,2*pi,numpasos2);
+h1 = waitbar(0,'Calculando...');
+hold off
 for n=1:numpasos1
     g = gamma(n);
     for m=1:numpasos2
         fi = phi(m);
-        x(n,m)=senal(fi,1,p,lambda,g,z2,tam);
-        y(n,m)=senal(fi,2,p,lambda,g,z2,tam);
-        
+        x(n,m)=abs(senal(fi,1,p,lambda,g,z2,tam));
+        y(n,m)=abs(senal(fi,2,p,lambda,g,z2,tam));
         if n==1
             lowe(m,1)=x(n,m);
             lowe(m,2)=y(n,m);
@@ -45,7 +50,31 @@ for n=1:numpasos1
             tope(m,2)=y(n,m);
         end
     end
-    
-    eli(n)=elipticidad(x,y,phi);
+    %plot(x(n,:),y(n,:));
+    [eli(n),eps(n)]=elipticidad(x(n,:),y(n,:),phi);
+    waitbar(n/numpasos1,h1);
 end
+close(h1);
+h = figure;
+hold on
+plot(lowe(:,1),lowe(:,2))
+plot(mide(:,1),mide(:,2))
+plot(tope(:,1),tope(:,2))
+title('Figuras de Lissajous varias');
+leg1 = ['\Delta z =' num2str(-zl*1000000) ' \mu m'];
+leg2 = ['\Delta z = 0 \mu m'];
+leg3 = ['\Delta z =' num2str(zl*1000000) ' \mu m'];
+legend(leg1,leg2,leg3);
+xlabel('S_A (u.a.)');
+ylabel('S_B (u.a.)');
+hold off
+
+w = figure;
+hold on
+dz = dz*1000000;
+[fp, pf, we]=plotyy(dz,eli,dz,eps);
+ylabel(fp(1),'Elipticidad');
+ylabel(fp(2),'Eliptancia');
+title('Elipticidades');
+xlabel('Desenfoque (\mu m)');
     
--- numerical/senal.m
+++ numerical/senal.m
@@ -1,32 +1,21 @@
 function s = senal(phi,eje,p,lambda,gamma,z,tam)
     
     s = 0;
-    
     %Variables del problema
-    umbral = 1000;
     q = 2*pi./p;
     
-    w1 = waitbar(0,['Sumando para eje ' num2str(eje) '...']);
-    for n=-umbral:umbral
-        w2 = waitbar(0,['Sumando para n=' num2str(n) '...']);
-        for m=-umbral:umbral
-            w3 = waitbar(0,['Sumando para m=' num2str(m) '...']);
-            for r=-umbral:umbral
+    for n=[-3, -1, 0, 1, 3]
+        for m=[-3, -1, 0, 1, 3]
+            for r=[-3, -1, 0, 1, 3]
                 fac = 1;
                 fac = fac*spas(p,lambda,n,m,gamma,z);
                 fac = fac*fourcoef(n)*conj(fourcoef(m))*fourcoef(r);
                 fac = fac*exp(1i*r*(phi+eje*pi/2));
                 fac = fac*sinq(pi*tam*terminus(n,m,r,gamma)/p);
-                fac = fac*exp(1i*q*terminus(n,m,r,gamma)*tam/2);
+                fac = fac*exp(1i*q*terminus(n,m,r,gamma)*(tam/2+(eje-1)*tam));
+                fac = real(fac);
                 s = s + fac;
-                waitbar((r+umbral)/(2*umbral),w3);
             end
-            close(w3);
-            waitbar((m+umbral)/(2*umbral),w2);
         end
-        close(w2);
-        waitbar((n+umbral)/(2*umbral),w1);
     end
-    close(w1);
-
 end
